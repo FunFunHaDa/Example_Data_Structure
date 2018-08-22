49eef340a6fcdda9adc93a2a289fefb8099501bb
FunFunHaDa <kjhkjh0929@gmail.com>
2018-08-22 23:02:43 +0900

    11, (Data_Structure)

---
+++ 28_ExpressionTree.h
@@ -0,0 +1,13 @@
+//#ifdef __EXPRESSION_TREE_H__
+//#define __EXPRESSION_TREE_H__
+
+#include "27_BinaryTree2.h"
+
+BTreeNode * MakeExpTree(char exp[]);
+int EvaluateExpTree(BTreeNode * bt);
+
+void ShowPrefixTypeExp(BTreeNode * bt);
+void ShowInfixTypeExp(BTreeNode * bt);
+void ShowPostfixTypeExp(BTreeNode * bt);
+
+//#endif // __EXPRESSION_TREE_H__
