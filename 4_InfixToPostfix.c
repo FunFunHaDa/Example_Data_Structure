1f3a1ea895f2dcbf40d3c0ce9bf2aee7757cf946
FunFunHaDa <kjhkjh0929@gmail.com>
2018-09-25 16:30:34 +0900

    4_4, (Data_Structure)

--- 4_InfixToPostfix.c
+++ 4_InfixToPostfix.c
@@ -43,5 +43,42 @@ void ConvToRPNExp(char exp[])
 
 	memset(convExp, 0, sizeof(char)*expLen + 1);
 	StackInit(&stack);
-	///////
+
+	for (i = 0; i < expLen; i++)
+	{
+		tok = exp[i];
+		if (isdigit(tok))
+		{
+			convExp[idx++] = tok;
+		}
+		else
+		{
+			switch (tok)
+			{
+			case '(':
+				SPush(&stack, tok);
+				break;
+			case ')':
+				while (1)
+				{
+					popOp = SPop(&stack);
+					if (popOp == '(')
+						break;
+					convExp[idx++] = popOp;
+				}
+				break;
+			case '+': case '-':
+			case '*': case '/':
+				while (!SIsEmpty(&stack) && WhoPrecOp(SPeek(&stack), tok) >= 0)
+					convExp[idx++] = SPop(&stack);
+				SPush(&stack, tok);
+				break;
+			}
+		}
+	}
+	while (!SIsEmpty(&stack))
+		convExp[idx++] = SPop(&stack);
+
+	strcpy(exp, convExp);
+	free(convExp);
 }
