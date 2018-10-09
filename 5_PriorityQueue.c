a680f355550f8345dfc5a0d4f6d3858a6fbeebf6
FunFunHaDa <kjhkjh0929@gmail.com>
2018-10-09 18:28:40 +0900

    5_6, (Data_Structure)

---
+++ 5_PriorityQueue.c
@@ -0,0 +1,22 @@
+#include "5_PriorityQueue.h"
+#include "5_UsefulHeap.h"
+
+void PQueueInit(PQueue * ppq, PriorityComp pc)
+{
+	HeapInit(ppq, pc);
+}
+
+int PQIsEmpty(PQueue * ppq)
+{
+	return HIsEmpty(ppq);
+}
+
+void PEnqueue(PQueue * ppq, PQData data)
+{
+	HInsert(ppq, data);
+}
+
+PQData PDequeue(PQueue * ppq)
+{
+	return HDelete(ppq);
+}
