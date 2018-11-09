1827d555e693a3a79167a298f0e3801d653f9a55
FunFunHaDa <kjhkjh0929@gmail.com>
2018-11-09 20:48:59 +0900

    8_7, (Data_Structure)

---
+++ 8_Slot.h
@@ -0,0 +1,16 @@
+#ifndef __SLOT_H__
+
+#include "8_Person.h"
+
+typedef int Key;
+typedef Person * Value;
+
+enum SlotStatus {EMPTY, DELETED, INUSE};
+
+typedef struct _slot
+{
+	Key key;
+	Value val;
+	enum SlotStatus status;
+}Slot;
+#endif // !__SLOT_H__
