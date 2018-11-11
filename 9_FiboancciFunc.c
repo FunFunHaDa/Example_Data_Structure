ae37986cdfceeb318e0a76190f49d9787ff12e6c
FunFunHaDa <kjhkjh0929@gmail.com>
2018-11-11 14:46:58 +0900

    9_1, (Data_Structure) Ninth

---
+++ 9_FiboancciFunc.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int Fibo(int n)
+{
+	if (n == 1)
+		return 0;
+	else if (n == 2)
+		return 1;
+	else
+		return Fibo(n - 1) + Fibo(n - 2);
+}
+
+int Fibo2(int n)
+{
+	printf("func call param %d \n", n);
+
+	if (n == 1)
+		return 0;
+	else if (n == 2)
+		return 1;
+	else
+		return Fibo2(n - 1) + Fibo2(n - 2);
+}
+
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 15; i++)
+		printf("%d ", Fibo(i));
+
+	puts("");
+
+	Fibo2(7);
+
+	return 0;
+}
