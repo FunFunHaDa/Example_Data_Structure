4937af2e086ede95d03f8a5afdeb43e20c8b329b
FunFunHaDa <kjhkjh0929@gmail.com>
2018-12-02 23:53:19 +0900

    11_6, (Data_Structure)

--- 11_BinaryTree2.h
+++ 11_BinaryTree2.h
@@ -25,5 +25,11 @@ typedef void VisitFuncPtr(BTData data);
 void PreorderTraverse(BTreeNode * bt, VisitFuncPtr action);
 void InorderTraverse(BTreeNode * bt, VisitFuncPtr action);
 void PostorderTraverse(BTreeNode * bt, VisitFuncPtr action);
+////
+BTreeNode * RemoveLeftSubTree(BTreeNode * bt);
+BTreeNode * RemoveRightSubTree(BTreeNode * bt);
+void ChangeLeftSubTree(BTreeNode * main, BTreeNode * sub);
+void ChangeRightSubTree(BTreeNode * main, BTreeNode * sub);
+////
 
 #endif // !__BINARY_TREE2_H__
