4e3c3df2ac01861a94785d65c6161671fb9503f1
FunFunHaDa <kjhkjh0929@gmail.com>
2019-01-12 23:51:03 +0900

    16_6, (Data_Structure)

---
+++ 16_BinarySearchTree.h
@@ -0,0 +1,14 @@
+#ifndef __BINARY_SEARCH_TREE_H__
+#define __BINARY_SERACH_TREE_H__
+
+#include "16_BinaryTree.h"
+typedef BTData BSTData;
+
+void BSTMakeAndInit(BTreeNode ** pRoot);
+BSTData BSTGetNodeData(BTreeNode * bst);
+void BSTInsert(BTreeNode ** pRoot, BSTData data);
+BTreeNode * BSTSearch(BTreeNode * bst, BSTData target);
+
+BTreeNode * BSTRemove(BTreeNode ** pRoot, BSTData target);
+void BSTShowAll(BTreeNode * bst);
+#endif // !__BINARY_SEARCH_TREE_H__
