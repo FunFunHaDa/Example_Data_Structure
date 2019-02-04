1c22000133a6f0b7d57fabab44f3c5051ec4fdb3
FunFunHaDa <kjhkjh0929@gmail.com>
2019-02-04 18:21:00 +0900

    19_5, (Data_Structure)

---
+++ 19_PriorityQueue.c
@@ -0,0 +1,20 @@
+#include "19_PriorityQueue.h"
+#include "19_UsefulHeap.h"
+
+void PQueueInit(PQueue * ppq, PriorityComp pc)
+{
+	HeapInit(ppq, pc);
+}
+int PQIsEmpty(PQueue * ppq)
+{
+	return HIsEmpty(ppq);
+}
+
+void PEnqueue(PQueue * ppq, PQData data)
+{
+	HInsert(ppq, data);
+}
+PQData PDequeue(PQueue * ppq)
+{
+	return HDelete(ppq);
+}
