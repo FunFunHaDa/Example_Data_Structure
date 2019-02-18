f8a2267e0a217d35c3dd0891764010d7897e5216
FunFunHaDa <kjhkjh0929@gmail.com>
2019-02-18 12:12:49 +0900

    21_2, (Data_Structure)

--- 21_LinkedRead.c
+++ 21_LinkedRead.c
@@ -19,7 +19,7 @@ int main(void)
 	while (1)
 	{
 		printf("자연수 입력: ");
-		scnaf("%d", &readData);
+		scanf("%d", &readData);
 		if (readData < 1)
 			break;
 		newNode = (Node*)malloc(sizeof(Node));
@@ -35,5 +35,43 @@ int main(void)
 	}
 	printf("\n");
 
-	
+	printf("입력 받은 데이터의 전체출력! \n");
+	if (head == NULL)
+	{
+		printf("저장된 자연수가 존재하지 않습니다. \n");
+	}
+	else
+	{
+		cur = head;
+		printf("%d ", cur->data);
+		while (cur->next != NULL)
+		{
+			cur = cur->next;
+			printf("%d ", cur->data);
+		}
+	}
+	printf("\n\n");
+
+	if (head == NULL)
+	{
+		return 0;
+	}
+	else
+	{
+		Node * delNode = head;
+		Node * delNextNode = head->next;
+
+		printf("%d을(를) 삭제합니다. \n", head->data);
+		free(delNode);
+
+		while (delNextNode != NULL)
+		{
+			delNode = delNextNode;
+			delNextNode = delNextNode->next;
+
+			printf("%d을(를) 삭제합니다. \n", delNode->data);
+			free(delNode);
+		}
+	}
+	return 0;
 }
