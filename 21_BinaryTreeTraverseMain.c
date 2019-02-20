3b9156c3f1b267f097355ada5b597cf0f7978cbf
FunFunHaDa <kjhkjh0929@gmail.com>
2019-02-20 23:07:33 +0900

    21_4, (Data_Structure)

---
+++ 21_BinaryTreeTraverseMain.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include"21_BinaryTree.h"
+
+void InorderTreverse(BTreeNode * bt)
+{
+	if (bt == NULL)
+		return;
+
+	InorderTreverse(bt->left);
+	printf("%d \n", bt->data);
+	InorderTreverse(bt->right);
+}
+
+int main(void)
+{
+	BTreeNode * bt1 = MakeBTreeNode();
+	BTreeNode * bt2 = MakeBTreeNode();
+	BTreeNode * bt3 = MakeBTreeNode();
+	BTreeNode * bt4 = MakeBTreeNode();
+
+	SetData(bt1, 1);
+	SetData(bt2, 2);
+	SetData(bt3, 3);
+	SetData(bt4, 4);
+	MakeLeftSubTree(bt1, bt2);
+	MakeRightSubTree(bt1, bt3);
+	MakeLeftSubTree(bt2, bt4);
+	InorderTreverse(bt1);
+	return 0;
+}
