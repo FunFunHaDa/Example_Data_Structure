9f2e3c81f39ada0543de0320f0a4b0f56b4cae48
FunFunHaDa <kjhkjh0929@gmail.com>
2019-02-23 23:14:27 +0900

    21_7, (Data_Structure)

---
+++ 21_Slot.h
@@ -0,0 +1,17 @@
+#ifndef __SLOT_H__
+#define __SLOT_H__
+
+#include "21_Person.h"
+
+typedef int Key;
+typedef Person * Value;
+
+enum SlotStatus { EMPTY, DELETED, INUSE };
+
+typedef struct _slot
+{
+	Key key;
+	Value val;
+	enum Slotatus status;
+}Slot;
+#endif // !__SLOT_H__
