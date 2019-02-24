4c546bc8d84e759250182aeb414cf87e73f66d53
FunFunHaDa <kjhkjh0929@gmail.com>
2019-02-24 18:09:25 +0900

    21_8, (Data_Structure)

--- 21_UsefulHeap.h
+++ 21_UsefulHeap.h
@@ -1,12 +1,12 @@
 #ifndef __USEFUL_HEAP_H__
 #define __USEFUL_HEAP_H__
-
+#include "21_ALEdge.h"
 #define TRUE 1
 #define FALSE 0
 
 #define HEAP_LEN 100
-
-typedef int HData;
+typedef Edge HData;
+// typedef int HData;
 typedef int PriorityComp(HData d1, HData d2);
 
 typedef struct _heap
