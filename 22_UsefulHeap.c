9453d6d63e0593ed888be886c12b60ddc7bc049d
FunFunHaDa <kjhkjh0929@gmail.com>
2019-03-01 23:18:31 +0900

    22_5, (Data_Structure)

--- 22_UsefulHeap.c
+++ 22_UsefulHeap.c
@@ -1,8 +1,9 @@
-#include "22_SimpleHeap.h"
+#include "22_UsefulHeap.h"
 
-void HeapInit(Heap * ph)
+void HeapInit(Heap * ph, PriorityComp pc)
 {
 	ph->numOfData = 0;
+	ph->comp = pc;
 }
 int HIsEmpty(Heap * ph)
 {
@@ -27,45 +28,42 @@ int GetHiPriChildIDX(Heap * ph, int idx)
 {
 	if (GetLChildIDX(idx) > ph->numOfData)
 		return 0;
-	else if (GetLChildIDX(idx) > ph->numOfData)
+	else if (GetLChildIDX(idx) == ph->numOfData)
 		return GetLChildIDX(idx);
 	else
 	{
-		if (ph->heapArr[GetLChildIDX(idx)].pr > ph->heapArr[GetRChildIDX(idx)].pr)
+		if (ph->comp(ph->heapArr[GetLChildIDX(idx)], ph->heapArr[GetRChildIDX(idx)]) < 0)
 			return GetRChildIDX(idx);
 		else
-			return GetLChildIDX(idx); 
+			return GetLChildIDX(idx);
 	}
 }
-void HInsert(Heap * ph, HData data, Priority pr)
+void HInsert(Heap * ph, HData data)
 {
 	int idx = ph->numOfData + 1;
-	HeapElem nelem = { pr, data };
-
 	while (idx != 1)
 	{
-		if (pr < (ph->heapArr[GetParentIDX(idx)].pr))
+		if (ph->comp(data, ph->heapArr[GetParentIDX(idx)]) > 0)
 		{
-			ph->heapArr[idx] = ph->heapArr[GetParentIDX(idx)];
+			ph->heapArr[idx] = ph->heapArr[GetParentIDX(idx)];;
 			idx = GetParentIDX(idx);
 		}
 		else
 			break;
 	}
-	ph->heapArr[idx] = nelem;
+	ph->heapArr[idx] = data;
 	ph->numOfData += 1;
 }
 HData HDelete(Heap * ph)
 {
-	HData retData = (ph->heapArr[1]).data;
-	HeapElem lastElem = ph->heapArr[ph->numOfData];
-
+	HData retData = ph->heapArr[1];
+	HData lastElem = ph->heapArr[ph->numOfData];
 	int parentIdx = 1;
 	int childIdx;
 
 	while (childIdx = GetHiPriChildIDX(ph, parentIdx))
 	{
-		if (lastElem.pr <= ph->heapArr[childIdx].pr)
+		if (ph->comp(lastElem, ph->heapArr[childIdx]) >= 0)
 			break;
 		ph->heapArr[parentIdx] = ph->heapArr[childIdx];
 		parentIdx = childIdx;
