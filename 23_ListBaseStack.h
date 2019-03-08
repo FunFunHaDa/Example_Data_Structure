6651ba51b888d6f335d0bb0d35a49a59d41a5f61
FunFunHaDa <kjhkjh0929@gmail.com>
2019-03-08 23:53:50 +0900

    23_4, (Data_Structure)

--- 23_ListBaseStack.h
+++ 23_ListBaseStack.h
@@ -1,10 +1,12 @@
 #ifndef __LB_STACK_H__
 #define __LB_STACK_H__
-
+#include "23_BinaryTree.h"
 #define TRUE 1
 #define FALSE 0
 
-typedef int Data;
+// typedef int Data;
+typedef BTreeNode * Data;
+
 typedef struct _node
 {
 	Data data;
