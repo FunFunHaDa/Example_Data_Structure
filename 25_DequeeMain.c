c8fee29db5ff44c4342c2b4437b04b913599697b
FunFunHaDa <kjhkjh0929@gmail.com>
2019-03-24 20:22:15 +0900

    25_4, (Data_Structure)

---
+++ 25_DequeeMain.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include"25_Dequee.h"
+
+int main(void)
+{
+	Deque deq;
+	DequeInit(&deq);
+
+	DQAddFirst(&deq, 3);
+	DQAddFirst(&deq, 2);
+	DQAddFirst(&deq, 1);
+	DQAddLast(&deq, 4);
+	DQAddLast(&deq, 5);
+	DQAddLast(&deq, 6);
+
+	while (!DQIsEmpty(&deq))
+		printf("%d ", DQRemoveFirst(&deq));
+	printf("\n");
+
+	DQAddFirst(&deq, 3);
+	DQAddFirst(&deq, 2);
+	DQAddFirst(&deq, 1);
+	DQAddLast(&deq, 4);
+	DQAddLast(&deq, 5);
+	DQAddLast(&deq, 6);
+
+	while (!DQIsEmpty(&deq))
+		printf("%d ", DQRemoveLast(&deq));
+	return 0;
+}
