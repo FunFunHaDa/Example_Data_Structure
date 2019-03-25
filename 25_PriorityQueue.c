e303f85d9516dd77e6f12a732232cddc1e1da875
FunFunHaDa <kjhkjh0929@gmail.com>
2019-03-25 20:15:16 +0900

    25_5, (Data_Structure)

---
+++ 25_PriorityQueue.c
@@ -0,0 +1,20 @@
+#include "25_PriorityQueue.h"
+#include "25_UsefulHeap.h"
+
+void PQueueInit(PQueue * ppq, PriorityComp pc)
+{
+	HeapInit(ppq, pc);
+}
+int PQIsEmpty(PQueue * ppq)
+{
+	return HIsEmpty(ppq);
+}
+
+void PEnqueue(PQueue * ppq, PQData data)
+{
+	HInsert(ppq, data);
+}
+PQData PDequeue(PQueue * ppq)
+{
+	return HDelete(ppq);
+}
