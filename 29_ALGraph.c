066f3acce25a9e0183a9c4bd4b10fbed206a3bdf
FunFunHaDa <kjhkjh0929@gmail.com>
2019-05-04 23:55:38 +0900

    29_8, (Data_Structure)

---
+++ 29_ALGraph.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "29_ALGraph.h"
+#include "29_DLinkedList.h"
+
+int WhoIsPrecede(int data1, int data2);
+
+void GraphInit(ALGraph * pg, int nv)
+{
+	int i;
+
+	pg->ajdList = (List*)malloc(sizeof(List)*nv);
+	pg->numV = nv;
+	pg->numE = 0;
+
+	for (i = 0; i < nv; i++)
+	{
+		ListInit(&(pg->ajdList[i]));
+		SetSortRule(&(pg->ajdList[i]), WhoIsPrecede);
+	}
+}
+void GraphDestroy(ALGraph * pg)
+{
+	if (pg->ajdList != NULL)
+		free(pg->ajdList);
+}
+void AddEdge(ALGraph * pg, int fromV, int toV)
+{
+	LInsert(&(pg->ajdList[fromV]), toV);
+	LInsert(&(pg->ajdList[toV]), fromV);
+	pg->numE += 1;
+}
+void ShowGraphEdgeInfo(ALGraph * pg)
+{
+	int i;
+	int vx;
+
+	for (i = 0; i < pg->numV; i++)
+	{
+		printf("%c와 연결된 정점: ", i + 65);
+
+		if (LFirst(&(pg->ajdList[i]), &vx))
+		{
+			printf("%c ", vx + 65);
+
+			while (LNext(&(pg->ajdList[i]), &vx))
+				printf("%c ", vx + 65);
+		}
+		printf("\n");
+	}
+}
+
+int WhoIsPrecede(int data1, int data2)
+{
+	if (data1 < data2)
+		return 0;
+	else
+		return 1;
+}
--- 29_UsefulHeap.h
+++ 29_UsefulHeap.h
@@ -1,13 +1,15 @@
 #ifndef __USEFUL_HEAP_H__
 #define __USEFUL_HEAP_H__
-
+#include "29_ALEdge.h"
 #define TRUE 1
 #define FALSE 0
 
 #define HEAP_LEN 100
 
 // typedef char HData;
-typedef int HData;
+// typedef int HData;
+typedef Edge HData;
+
 
 typedef int PriorityComp(HData d1, HData d2);
 
